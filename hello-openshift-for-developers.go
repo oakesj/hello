60bc8285d23d8491a215582ef188b55d870246e5
oakesj <63422711+oakesj@users.noreply.github.com>
2023-04-17 11:09:33 -0700

    Update hello-openshift-for-developers.go

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello OpenShift for Developers!"
+		response = "Hello All you out there!"
 	}
 
 	fmt.Fprintln(w, response)
